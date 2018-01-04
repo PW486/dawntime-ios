5e094b848c5dbb6184c3812ec731741569e07cbd
PW486 <ooqwe486@gmail.com>
2018-01-04 20:48:14 +0900

    Add My Page

--- dawntime/ViewControllers/MyPage/MyPageViewController.swift
+++ dawntime/ViewControllers/MyPage/MyPageViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
-    var menus: [String] = ["내가 쓴 글", "스크랩", "쪽지", "찜 목록", "설정"]
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var profileView: UIView!
+    @IBOutlet weak var logInBtn: UIButton!
+    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet weak var logInRequireLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var userImage: UIImageView!
+    
+    var logInStatus = false
+    var menus: [String] = ["내가 쓴 글", "스크랩", "쪽지", "찜 목록", "설정", ""]
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if logInStatus {
+            logInBtn.isHidden = true
+            signUpBtn.isHidden = true
+            logInRequireLabel.isHidden = true
+            emailLabel.text = "logIn@logIn.com"
+            userImage.image = #imageLiteral(resourceName: "view_activeprofile_navy")
+        }
+        else {
+            emailLabel.isHidden = true
+            userImage.image = #imageLiteral(resourceName: "view_unactiveprofile_gray")
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        profileView.addBottomBorderWithColor(color: self.tableView.separatorColor!, width: 1.0)
+        
+        // tableView Footer line
+//        let px = 1 / UIScreen.main.scale
+//        let frame = CGRect(x: 12, y: 0, width: self.tableView.frame.size.width - 36, height: px)
+//        let line = UIView(frame: frame)
+//        self.tableView.tableFooterView = line
+//        line.backgroundColor = self.tableView.separatorColor
     }
 }
 
@@ -21,19 +52,33 @@ extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
         return 6
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // 프로필 부분 셀 말고 따로 빼기
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch(indexPath.row) {
         case 0:
-            let cell = tableView.dequeueReusableCell(withIdentifier: MyPageProfileTableViewCell.reuseIdentifier) as! MyPageProfileTableViewCell
-            cell.selectionStyle = .none
-            return cell
+            break
         default:
-            let cell = tableView.dequeueReusableCell(withIdentifier: MyPageMenuTableViewCell.reuseIdentifier) as! MyPageMenuTableViewCell
-            cell.menuLabel.text = menus[indexPath.row]
-            return cell
+            print("default")
         }
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MyPageMenuTableViewCell.reuseIdentifier) as! MyPageMenuTableViewCell
+        
+        cell.selectionStyle = .none
+        cell.menuLabel.text = menus[indexPath.row]
+        if logInStatus && menus[indexPath.row] == "찜 목록" {
+            cell.menuLabel.textColor = UIColor.hexStringToUIColor(hex: "#ED508E")
+        }
+        else if logInStatus {
+            cell.menuLabel.textColor = UIColor.hexStringToUIColor(hex: "#0E1949")
+        }
+        else {
+            cell.menuLabel.textColor = UIColor.hexStringToUIColor(hex: "#B7B7B7")
+        }
+        return cell
+    }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 55
+    }
 }
